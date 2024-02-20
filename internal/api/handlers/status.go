4f8505a52e130655c99c26378d28ce02656bbdd1
Ivan Dominguez <ivan.dominguez@e-cruce.com>
2024-02-20 16:07:09 -0300

    gitlab secret

--- internal/api/handlers/status.go
+++ internal/api/handlers/status.go
@@ -3,5 +3,5 @@ package handler
 import "github.com/gofiber/fiber/v2"
 
 func Status(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"status": "success", "message": "Hello i'm ok!!", "data": nil})
+	return c.JSON(fiber.Map{"status": "success", "message": "Hello i'm ok!", "data": nil})
 }
